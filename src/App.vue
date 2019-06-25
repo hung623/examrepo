9d0c42f5b16e3ac371a5271195db110851914c0c
hung van tran <hung@623.local>
2019-06-25 13:57:41 +0200

    added files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>RG lunch list</p>
+    <li v-for="people in peoples, sortedPeople" :key="people.id">
+    {{ people.name }}
+    {{ formatString(people.counter) }}
+    <button v-if="people.edit = true" @click=" counter += 1">Made lunch!</button>
+    </li>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data: function() {
+    return {
+      peoples: [],
+      counter: 0,
+      failure: ""
+    };
+  },
+  computed: {
+    sortedPeople: function() {
+      return this.counter.sort();
+    }
+  },
+  methods: {
+    getPeople: function() {
+      let self = this;
+      axios
+        .get("https://api.jsonbin.io/b/5d11fe72c8516d4ebcbb3b16/1")
+        .then(function(response) {
+          // handle success
+          console.log(response);
+          self.peoples = response.data;
+        })
+        .catch(function(error) {
+          // handle error
+          console.log(error);
+        })
+        .then(function() {
+          // always executed
+        });
+     },
+     updateData: function() {
+      let self = this;
+      axios
+        .put("https://api.jsonbin.io/b/5d11fe72c8516d4ebcbb3b16/1", self.peoples)
+        .then(function(response) {
+          // handle success
+          console.log(response);
+          console.log("it works!");
+        })
+        .catch(function(error) {
+          // handle error
+          console.log(error);
+        })
+        .then(function() {
+          // always executed
+        });
+    },
+    formatString: function() {
+      return parseInt(this.counter)
+    },
+  },   
+  beforeMount() {
+    this.getPeople();
+    // this runs before the page renders and will grab people from json list
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
